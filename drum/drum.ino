8e9fe595f17fa0c8e21a416fb50936cc21382fc2
Viksaaan <viksaaan@gmail.com>
2019-04-22 09:58:07 +0300

    modifide

--- drum/drum.ino
+++ drum/drum.ino
@@ -2,31 +2,49 @@
 #define PADS 9      // количество дрампадов
 #define BAUD_RATE 115200  // скорость работы Монитора порта
 
-unsigned long timer[PADS];
-bool playing[PADS];      
-int highScore[PADS];
+/* Settings
+ *  
+   (36) Kick     0|1
+   (38) Snare    1|2
+   (71) Tom 1    2|4
+   (69) Tom 2    3|3
+   (67) Tom 3    4|0
 
+   (51) HiHat     5|8
+   (46) Cymbal 1  6|5
+   (79) Cymbal 2  7|7
+   (45) Ride      8|6
+
+*/
 
 
 // настройка дрампадов
 /*
-                            -T3
-                            |A0   -Kick
-                            |     |A1   -Snare
-                            |     |     |A2   -T2
-                            |     |     |     |A3   -T1
-                            |     |     |     |     |A4   -C1
-                            |     |     |     |     |     |A5   -Ride
-                            |     |     |     |     |     |     |A6   -C2
-                            |     |     |     |     |     |     |     |A7   -HH
-                            |     |     |     |     |     |     |     |     |A8 
+                         -T3
+                         |A0   -Kick
+                         |     |A1   -Snare
+                         |     |     |A2   -T2
+                         |     |     |     |A3   -T1
+                         |     |     |     |     |A4   -C1
+                         |     |     |     |     |     |A5   -Ride
+                         |     |     |     |     |     |     |A6   -C2
+                         |     |     |     |     |     |     |     |A7   -HH
+                         |     |     |     |     |     |     |     |     |A8 
 */        
-byte note[PADS]         = { 67,   36,   38,   69,   71,   46,   45,   79,   51};
+byte note[PADS]      = { 67,   36,   38,   69,   71,   46,   45,   79,   51};
 
-short thresholdMin[PADS]= { 100,  100,  100,  100,  100,  100,  300,  300,  300};    // Нижний порог чувствительности                  
-short thresholdMax[PADS]= { 400,  400,  400,  400,  400,  400,  1000, 1000, 1000};   // Верхний порог чувствительности                      
-byte sensorScantime[PADS]={ 10,   10,   10,   10,   10,   10,   10,   10,   10};     // Частота опроса датчиков (мс)
-byte sensorMasktime[PADS]={ 20,   20,   20,   20,   20,   20,   20,   20,   20};     // Длительность игнорирования датчика после получения с него сигнала (мс)
+short minLimit[PADS] = { 100,  100,  100,  100,  100,  100,  300,  300,  300};    // Нижний порог чувствительности                  
+short maxLimit[PADS] = { 400,  400,  400,  400,  400,  400,  1000, 1000, 1000};   // Верхний порог чувствительности                      
+byte scanTime[PADS]  = { 10,   10,   10,   10,   10,   10,   10,   10,   10};     // Частота опроса датчиков (мс)
+byte maskTime[PADS]  = { 20,   20,   20,   20,   20,   20,   20,   20,   20};     // Длительность игнорирования датчика после получения с него сигнала (мс)
+
+//bool playing[PADS]       = {false,false,false,false,false,false,false,false,false};
+//int highScore[PADS]      = {0,     0,    0,    0,    0,    0,    0,    0,    0};
+//unsigned long timer[PADS]= {0,     0,    0,    0,    0,    0,    0,    0,    0};
+
+unsigned long timer[PADS];
+bool playing[PADS];      
+int highScore[PADS];
 
 /*
       
@@ -51,18 +69,18 @@ void loop() {
     short volume = analogRead(i);
 
     // если считанное значение превышает Минимальное пороговое значение
-    if (volume >= thresholdMin[i] && playing[i] == false) {
-      if (millis() - timer[i] >= sensorScantime[i]) {
+    if (volume >= minLimit[i] && playing[i] == false) {
+      if (millis() - timer[i] >= scanTime[i]) {
         playing[i] = true;
         playNote(i, volume);
       }
     }
-    else if (volume >= thresholdMin[i] && playing[i] == true) {
+    else if (volume >= minLimit[i] && playing[i] == true) {
       playNote(i, volume);
     }
-    else if (volume < thresholdMin[i] && playing[i] == true) {
-      noteOn(0x91, note[i], highScore[i], sensorMasktime[i]);
-      noteOff(0x91, note[i], 0, sensorMasktime[i]);
+    else if (volume < minLimit[i] && playing[i] == true) {
+      noteOn(0x91, note[i], highScore[i], maskTime[i]);
+      noteOff(0x91, note[i], 0, maskTime[i]);
       highScore[i] = 0;
       playing[i] = false;
       timer[i] = millis();  // прошло мс с начала работы устройства
@@ -72,7 +90,7 @@ void loop() {
 
 // функция выравнивания силы звучания
 void playNote (byte pad, short volume) {
-  float velocity = ((volume) / float(thresholdMax[pad] - thresholdMin[pad])) * 200;
+  float velocity = ((volume) / float(maxLimit[pad] - minLimit[pad])) * 200;
   
   if (velocity > 127) 
     velocity = 127;
